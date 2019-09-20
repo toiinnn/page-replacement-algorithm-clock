2558366f8ee55b9d0769ff9ee387af110c3ac0dd
Andrecio Bezerra <andreciobezerra@gmail.com>
2019-09-20 12:08:35 -0300

    Verification on bit R OK!

--- clock.py
+++ clock.py
@@ -2,31 +2,36 @@ from itertools import cycle
 import time
 
 list = []
-cycleList = cycle(list)        
-
 
 def insertPage(page):
     list.append(page)
 
 def listPages():
-    count = 1
-    for page in cycleList:
-        print("Página atual: {0} bit R {1}".format(count, page[0]))
-        count = count + 1
+    for index,page in enumerate(list):
+        print("Página atual: {0} bit R {1}".format(index+1, page[0]))
+    print('\n')
 
+def verify():
+    for index,page in enumerate(cycle(list)):
+        if(page[0]==0):
+            return index
+        else:
+            page[0] = 0
 
-
+#falta criar a Thread que verificará os 10seg automicamente
 def main():       
-    #pointer = cycleList(0)
     while(True):
-        option = input("Deseja fazer o que?\n (1) Inserir página \n (2) Listar páginas")
+        option = input("Deseja fazer o que?\n (1) Inserir página \n (2) Listar páginas\n (3) Sair\n \nSua opção:")
         if(option == '1'):
             if(len(list) <= 10):
-                insertPage((1,time.time()))
+                insertPage((1,time.time())) 
             else:
-                
+                index= verify()
+                list[index] = (1,time.time())                
         elif (option == '2'):
             listPages()
+        elif (option == '3'):
+            break
         else:
             continue
 
