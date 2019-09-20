7474c612ef10eade8d5586e2dc3fe85d49e6ed71
Antonio Marcos <anthoin@ufrn.edu.br>
2019-09-20 17:48:03 -0300

    Atualização: atualize, listpages e verify Adição: menu

--- clock.py
+++ clock.py
@@ -1,38 +1,48 @@
 from itertools import cycle
 import time
+import threading
 
 list = []
 
 def listPages():
     for index,page in enumerate(list):
-        print("Página atual: {0} bit R {1}".format(index+1, page[0]))
+        print("\nPágina atual: {0} bit R {1}".format(index+1, page[0]))
     print('\n')
 
 def verify():
-    for index,page in enumerate(cycle(list)):
+    for page in cycle(list):
         if(page[0]==0):
-            return index
+            return page
         else:
-            page[0] = 0
+            page[0]=0
 
-#falta criar a Thread que verificará os 10 seg automicamente!
-def main():       
+def atualize():
     while(True):
-        option = input("Deseja fazer o que?\n (1) Inserir página \n (2) Listar páginas\n (3) Sair\n \nSua opção:")
-        if(option == '1'):
-            if(len(list) <= 10):
-                list.append((1,time.time()))
-            else:
-                index= verify()
-                list[index] = (1,time.time())                
-        elif (option == '2'):
-            listPages()
-        elif (option == '3'):
-            break
+        time.sleep(10)
+        for index, page in enumerate(list):
+            if(page[0]==1):       
+                print("\nPágina {0} teve seu bit R zerado!".format(index+1))
+                page[0]=0         
+        menu()
+
+def menu():
+    option = input("Deseja fazer o que?\n (1) Inserir página \n (2) Listar páginas \nSua opção:")
+    if(option == '1'):
+        if(len(list) < 10):
+            list.append([1,time.time()])
         else:
-            continue
+            element = verify()
+            list.remove(element)
+            list.append([1,time.time()])                
+    elif (option == '2'):
+        listPages()
+
+def main():  
+    toAtualize = threading.Thread(target=atualize,args=[])     
+    toAtualize.start()
+    while(True):
+        menu()
 
 main()  
-
 
 
