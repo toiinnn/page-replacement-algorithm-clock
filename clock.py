2e6562cdf75c632409a96f0083c52377ab03a8a9
Andrecio Bezerra <andreciobezerra@gmail.com>
2019-09-20 12:14:35 -0300

    Refactoring

--- clock.py
+++ clock.py
@@ -3,9 +3,6 @@ import time
 
 list = []
 
-def insertPage(page):
-    list.append(page)
-
 def listPages():
     for index,page in enumerate(list):
         print("Página atual: {0} bit R {1}".format(index+1, page[0]))
@@ -24,7 +21,7 @@ def main():
         option = input("Deseja fazer o que?\n (1) Inserir página \n (2) Listar páginas\n (3) Sair\n \nSua opção:")
         if(option == '1'):
             if(len(list) <= 10):
-                insertPage((1,time.time())) 
+                list.append((1,time.time()))
             else:
                 index= verify()
                 list[index] = (1,time.time())                
